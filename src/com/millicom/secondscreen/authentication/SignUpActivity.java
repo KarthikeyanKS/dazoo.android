8a40584a046f6df5708be4f7b3c8cadc06413877
mcede <mattec92@hotmail.com>
2013-10-30 15:23:38 +0100

    Sign up with email

--- src/com/millicom/secondscreen/authentication/SignUpActivity.java
+++ src/com/millicom/secondscreen/authentication/SignUpActivity.java
@@ -23,10 +23,13 @@ import com.millicom.secondscreen.content.homepage.HomePageActivity;
 import com.millicom.secondscreen.utilities.JSONUtilities;
 import com.millicom.secondscreen.utilities.PatternCheck;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
@@ -36,6 +39,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -47,7 +51,13 @@ public class SignUpActivity extends ActionBarActivity implements OnClickListener
 	private Button				mDazooRegisterButton;
 	private String				userEmailRegister, userPasswordRegister, userFirstNameRegister, userLastNameRegister, dazooToken;
 	private TextView			mErrorTextView;
-	
+	private Drawable 			mFirstNameDrawable, mLastNameDrawable, mPasswordRegisterDrawable, mEmailRegisterDrawable;
+
+	private final int 			REGISTER_NAME_MISSING = 0;
+	private final int 			REGISTER_EMAIL_WRONG = 1;
+	private final int 			PASSWORD_LENGTH_WRONG = 2;
+	private final int 			PASSWORD_ILLEGAL_CHARACTERS = 3;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -75,6 +85,11 @@ public class SignUpActivity extends ActionBarActivity implements OnClickListener
 		mErrorTextView = (TextView) findViewById(R.id.signup_error_textview);
 		mDazooRegisterButton = (Button) findViewById(R.id.signup_register_button);
 		mDazooRegisterButton.setOnClickListener(this);
+		
+		mFirstNameDrawable = mFirstNameEditText.getBackground();
+		mLastNameDrawable = mLastNameEditText.getBackground();
+		mEmailRegisterDrawable = mEmailRegisterEditText.getBackground();
+		mPasswordRegisterDrawable = mPasswordRegisterEditText.getBackground();
 	}
 
 	private boolean verifyRegisterInput() {
@@ -82,13 +97,34 @@ public class SignUpActivity extends ActionBarActivity implements OnClickListener
 		String firstNameInput = mFirstNameEditText.getText().toString();
 		String lastNameInput = mLastNameEditText.getText().toString();
 		String passwordInput = mPasswordRegisterEditText.getText().toString();
-
-		if ((firstNameInput != null) && (lastNameInput != null) && (passwordInput != null) && (emailInput != null) && (passwordInput.length() >= Consts.MILLICOM_SECONSCREEN_PASSWORD_LENGTH_MIN)
+		if ((!firstNameInput.equals("")) && (!lastNameInput.equals("")) && (!passwordInput.equals("")) && (!emailInput.equals("")) && (passwordInput.length() >= Consts.MILLICOM_SECONSCREEN_PASSWORD_LENGTH_MIN)
 				&& (passwordInput.length() <= Consts.MILLICOM_SECONSCREEN_PASSWORD_LENGTH_MAX) && (!passwordInput.matches("[%,#/|<>]+")) && (PatternCheck.checkEmail(emailInput) == true)) {
 			return true;
 		} else return false;
 	}
-	
+
+	private int findWrongRegisterInput() {
+		String emailInput = mEmailRegisterEditText.getText().toString();
+		String firstNameInput = mFirstNameEditText.getText().toString();
+		String lastNameInput = mLastNameEditText.getText().toString();
+		String passwordInput = mPasswordRegisterEditText.getText().toString();
+		if ((firstNameInput.equals("")) || (lastNameInput.equals(""))) {
+			return REGISTER_NAME_MISSING;
+		}
+		else if ((emailInput.equals("")) || (PatternCheck.checkEmail(emailInput) == false)) {
+			return REGISTER_EMAIL_WRONG;
+		}
+		else if ((passwordInput.length() <= Consts.MILLICOM_SECONSCREEN_PASSWORD_LENGTH_MIN)
+				|| (passwordInput.length() >= Consts.MILLICOM_SECONSCREEN_PASSWORD_LENGTH_MAX)) {
+			return PASSWORD_LENGTH_WRONG;
+		}
+		else if (passwordInput.matches("[%,#/|<>]+")) {
+			return PASSWORD_ILLEGAL_CHARACTERS;
+		}
+		return -1;
+	}
+
+	@SuppressLint("NewApi")
 	@Override
 	public void onClick(View v) {
 		int id = v.getId();
@@ -122,13 +158,13 @@ public class SignUpActivity extends ActionBarActivity implements OnClickListener
 							String userDataString = dazooRegJSON.optString(Consts.MILLICOM_SECONDSCREEN_API_USER);
 							if (JSONUtilities.storeUserInformation(this, userDataString)) {
 								Toast.makeText(getApplicationContext(), "Hello, " + ((SecondScreenApplication) getApplicationContext()).getUserFirstName(), Toast.LENGTH_SHORT).show();
-								
+
 								// go to Start page
 								Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
 								startActivity(intent);
 								overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
 								finish();
-							
+
 							} else {
 								Toast.makeText(getApplicationContext(), "Failed to fetch the user information from backend", Toast.LENGTH_SHORT).show();
 							}
@@ -151,8 +187,70 @@ public class SignUpActivity extends ActionBarActivity implements OnClickListener
 					e.printStackTrace();
 				}
 			} else {
-//				Toast.makeText(getApplicationContext(), "check if email/password/name were input right", Toast.LENGTH_LONG).show();
-				mErrorTextView.setText("Check if email/password/name were input right!");
+				int errorId = findWrongRegisterInput();
+				//TODO: Fix compatibility with older api versions
+				if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+					if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+						if (mFirstNameEditText.getBackground() != mFirstNameDrawable) {
+							mFirstNameEditText.setBackground(mFirstNameDrawable);
+						}
+						if (mLastNameEditText.getBackground() != mLastNameDrawable) {
+							mLastNameEditText.setBackground(mLastNameDrawable);
+						}
+						if (mEmailRegisterEditText.getBackground() != mEmailRegisterDrawable) {
+							mEmailRegisterEditText.setBackground(mEmailRegisterDrawable);
+						}
+						if (mPasswordRegisterEditText.getBackground() != mPasswordRegisterDrawable) {
+							mPasswordRegisterEditText.setBackground(mPasswordRegisterDrawable);
+						}
+					}
+					switch (errorId) {
+						case REGISTER_NAME_MISSING:
+							mFirstNameEditText.setActivated(true);
+							mLastNameEditText.setActivated(true);
+							if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+								Drawable bg = getResources().getDrawable(R.drawable.edittext_activated);
+								mFirstNameEditText.setBackground(bg);
+								mLastNameEditText.setBackground(bg);
+							}
+							break;
+						case REGISTER_EMAIL_WRONG:
+							mEmailRegisterEditText.setActivated(true);
+							if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+								Drawable bg = getResources().getDrawable(R.drawable.edittext_activated);
+								mEmailRegisterEditText.setBackground(bg);
+							}
+							break;
+						case PASSWORD_LENGTH_WRONG:
+							mPasswordRegisterEditText.setActivated(true);
+							if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+								Drawable bg = getResources().getDrawable(R.drawable.edittext_activated);
+								mPasswordRegisterEditText.setBackground(bg);
+							}
+							break;
+						case PASSWORD_ILLEGAL_CHARACTERS:
+							mPasswordRegisterEditText.setActivated(true);
+							if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+								Drawable bg = getResources().getDrawable(R.drawable.edittext_activated);
+								mPasswordRegisterEditText.setBackground(bg);
+							}
+							break;
+					}
+				}
+				switch (errorId) {
+					case REGISTER_NAME_MISSING:
+						mErrorTextView.setText(getResources().getString(R.string.signup_with_email_error_name));
+						break;
+					case REGISTER_EMAIL_WRONG:
+						mErrorTextView.setText(getResources().getString(R.string.signup_with_email_error_email));
+						break;
+					case PASSWORD_LENGTH_WRONG:
+						mErrorTextView.setText(getResources().getString(R.string.signup_with_email_error_passwordlength));
+						break;
+					case PASSWORD_ILLEGAL_CHARACTERS:
+						mErrorTextView.setText(getResources().getString(R.string.signup_with_email_error_passwordcharacters));
+						break;
+				}
 				mErrorTextView.setVisibility(TextView.VISIBLE);
 				mFirstNameEditText.setEnabled(true);
 				mLastNameEditText.setEnabled(true);
@@ -162,7 +260,7 @@ public class SignUpActivity extends ActionBarActivity implements OnClickListener
 			break;
 		}
 	}
-	
+
 	private class DazooRegistrationTask extends AsyncTask<String, Void, String> {
 
 		@Override
@@ -198,8 +296,8 @@ public class SignUpActivity extends ActionBarActivity implements OnClickListener
 			return Consts.EMPTY_STRING;
 		}
 	}
-	
-	
+
+
 	@Override
 	public void onBackPressed() {
 		super.onBackPressed();
