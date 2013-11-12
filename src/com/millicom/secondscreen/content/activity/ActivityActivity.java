eec39a93c8dada111260a96e3f31c170454a3940
Anna Babaryka <anna.babaryka@idealapps.se>
2013-11-12 09:55:16 +0100

    sketch of the list view with the feed items from the backend

--- src/com/millicom/secondscreen/content/activity/ActivityActivity.java
+++ src/com/millicom/secondscreen/content/activity/ActivityActivity.java
@@ -1,40 +1,58 @@
 package com.millicom.secondscreen.content.activity;
 
+import java.util.ArrayList;
+
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.millicom.secondscreen.Consts.REQUEST_STATUS;
 import com.millicom.secondscreen.R;
+import com.millicom.secondscreen.SecondScreenApplication;
+import com.millicom.secondscreen.adapters.ActivityFeedListAdapter;
+import com.millicom.secondscreen.content.SSActivity;
 import com.millicom.secondscreen.content.SSPageFragmentActivity;
+import com.millicom.secondscreen.content.feed.FeedService;
 import com.millicom.secondscreen.content.homepage.HomeActivity;
+import com.millicom.secondscreen.content.model.FeedItem;
 import com.millicom.secondscreen.content.myprofile.MyProfileActivity;
 import com.millicom.secondscreen.content.search.SearchPageActivity;
+import com.millicom.secondscreen.http.NetworkUtils;
 
-public class ActivityActivity extends SSPageFragmentActivity implements OnClickListener {
+public class ActivityActivity extends SSActivity implements OnClickListener {
 
-	private static final String	TAG	= "ActivityActivity";
+	private static final String	TAG			= "ActivityActivity";
 	private TextView			mTxtTabTvGuide, mTxtTabPopular, mTxtTabFeed;
 	private ActionBar			mActionBar;
-
+	private ArrayList<FeedItem>	activityFeed;
+	private String				token;
+	private Boolean				mIsLoggenIn	= false;
+	private ListView mListView;
+	private ActivityFeedListAdapter mAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_activity_activity);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-		initViews();
+		token = ((SecondScreenApplication) getApplicationContext()).getAccessToken();
+		if (token != null && TextUtils.isEmpty(token) != true) {
+			mIsLoggenIn = true;
+		}
 
-		// TO BE DETERMINED LATER
-		// loadPage();
+		initViews();
+		loadPage();
 	}
 
 	private void initViews() {
@@ -51,30 +69,42 @@ public class ActivityActivity extends SSPageFragmentActivity implements OnClickL
 
 		mActionBar = getSupportActionBar();
 
-		final int actionBarColor = getResources().getColor(R.color.lightblue);
-		mActionBar.setBackgroundDrawable(new ColorDrawable(actionBarColor));
-
 		mActionBar.setDisplayShowTitleEnabled(true);
 		mActionBar.setDisplayShowCustomEnabled(true);
 		mActionBar.setDisplayUseLogoEnabled(true);
 		mActionBar.setDisplayShowHomeEnabled(true);
 		mActionBar.setTitle(getResources().getString(R.string.activity_title));
 
+		mListView = (ListView) findViewById(R.id.listview);
+		
 		// DO THE CALLBACK AND LOADING FUNCTIONALITY AND BEHAVIOR
-		// super.initCallbackLayouts();
+		super.initCallbackLayouts();
 	}
 
 	@Override
 	protected void loadPage() {
-	}
+		updateUI(REQUEST_STATUS.LOADING);
+		// check if the network connection exists
+		if (!NetworkUtils.checkConnection(this)) {
+			updateUI(REQUEST_STATUS.FAILED);
+		}
 
-	@Override
-	protected boolean pageHoldsData() {
-		return false;
+		activityFeed = FeedService.getActivityFeed(token);
+		if (activityFeed != null) {
+			if (activityFeed.isEmpty() != true) {
+				updateUI(REQUEST_STATUS.SUCCESSFUL);
+			}
+		} else {
+			updateUI(REQUEST_STATUS.EMPTY_RESPONSE);
+		}
 	}
 
 	@Override
 	protected void updateUI(REQUEST_STATUS status) {
+		if (super.requestIsSuccesfull(status)) {
+			mAdapter = new ActivityFeedListAdapter(this, activityFeed);
+			mListView.setAdapter(mAdapter);
+		}
 	}
 
 	@Override
@@ -98,7 +128,7 @@ public class ActivityActivity extends SSPageFragmentActivity implements OnClickL
 		inflater.inflate(R.menu.menu_homepage, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		int id = v.getId();
@@ -106,7 +136,7 @@ public class ActivityActivity extends SSPageFragmentActivity implements OnClickL
 		case R.id.show_tvguide:
 			// tab to home page
 			Intent intentHome = new Intent(ActivityActivity.this, HomeActivity.class);
-			intentHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); 
+			intentHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			intentHome.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			startActivity(intentHome);
 			break;
