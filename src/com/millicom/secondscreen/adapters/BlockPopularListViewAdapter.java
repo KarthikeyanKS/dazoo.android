7f648fbf4e8406463f45135352d5f8100fde5dda
Anna Babaryka <anna.babaryka@idealapps.se>
2013-11-13 10:06:00 +0100

    Activity: popular block xml and adapter

--- src/com/millicom/secondscreen/adapters/BlockPopularListViewAdapter.java
+++ src/com/millicom/secondscreen/adapters/BlockPopularListViewAdapter.java
@@ -20,20 +20,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class BlockPopularListViewAdapter extends BaseAdapter{
-	
-	private static final String TAG = "BlockPopularListViewAdapter";
-	
-	private LayoutInflater		mLayoutInflater;
-	private Activity mActivity;
-	private ArrayList<Broadcast> mPopularBroadcasts;
-	private ImageLoader			mImageLoader;
-	
-	public BlockPopularListViewAdapter(Activity activity, ArrayList<Broadcast> popularBroadcasts){
+public class BlockPopularListViewAdapter extends BaseAdapter {
+
+	private static final String		TAG	= "BlockPopularListViewAdapter";
+
+	private LayoutInflater			mLayoutInflater;
+	private Activity				mActivity;
+	private ArrayList<Broadcast>	mPopularBroadcasts;
+	private ImageLoader				mImageLoader;
+
+	public BlockPopularListViewAdapter(Activity activity, ArrayList<Broadcast> popularBroadcasts) {
 		this.mActivity = activity;
 		this.mPopularBroadcasts = popularBroadcasts;
 		this.mImageLoader = new ImageLoader(mActivity, R.drawable.loadimage);
@@ -41,14 +42,14 @@ public class BlockPopularListViewAdapter extends BaseAdapter{
 
 	@Override
 	public int getCount() {
-		if (mPopularBroadcasts!=null){
+		if (mPopularBroadcasts != null) {
 			return mPopularBroadcasts.size();
 		} else return 0;
 	}
 
 	@Override
 	public Broadcast getItem(int position) {
-		if (mPopularBroadcasts!=null){
+		if (mPopularBroadcasts != null) {
 			return mPopularBroadcasts.get(position);
 		} else return null;
 	}
@@ -61,23 +62,60 @@ public class BlockPopularListViewAdapter extends BaseAdapter{
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View rowView = convertView;
-		
+
 		final Broadcast broadcast = getItem(position);
-		
+
 		if (rowView == null) {
 			mLayoutInflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			rowView = mLayoutInflater.inflate(R.layout.block_feed_popular_listitem, null);
 			ViewHolder viewHolder = new ViewHolder();
-			
+			viewHolder.mContainer = (LinearLayout) rowView.findViewById(R.id.block_popular_feed_container);
+			viewHolder.mPoster = (ImageView) rowView.findViewById(R.id.block_feed_popular_listitem_iv);
+			viewHolder.mImageProgressBar = (ProgressBar) rowView.findViewById(R.id.block_feed_popular_listitem_iv_progressbar);
+			viewHolder.mIcon = (ImageView) rowView.findViewById(R.id.block_popular_feed_details_icon);
+			viewHolder.mTitle = (TextView) rowView.findViewById(R.id.block_popular_feed_details_title_tv);
+			viewHolder.mTime = (TextView) rowView.findViewById(R.id.block_popular_feed_details_time_tv);
+			viewHolder.mChannelName = (TextView) rowView.findViewById(R.id.block_popular_feed_details_channel_tv);
+			viewHolder.mDetails = (TextView) rowView.findViewById(R.id.block_popular_feed_details_extra_tv);
+			viewHolder.mProgressBarTitle = (TextView) rowView.findViewById(R.id.block_popular_feed_timeleft_tv);
+			viewHolder.mProgressBar = (ProgressBar) rowView.findViewById(R.id.block_popular_feed_progressbar);
 			rowView.setTag(viewHolder);
 		}
 
 		ViewHolder holder = (ViewHolder) rowView.getTag();
-		
-		if(broadcast!=null){
-		
+
+		if (broadcast != null) {
+			// different details about the broadcast program depending on the type
+			String programType = broadcast.getProgram().getProgramType();
+
+			mImageLoader.displayImage(broadcast.getProgram().getPosterMUrl(), holder.mPoster, ImageLoader.IMAGE_TYPE.POSTER);
+			if (Consts.DAZOO_PROGRAM_TYPE_TV_EPISODE.equals(programType)) {
+				holder.mTitle.setText(broadcast.getProgram().getSeries().getName());
+			} else {
+				holder.mTitle.setText(broadcast.getProgram().getTitle());
+			}
+			try {
+				holder.mTime.setText(DateUtilities.isoStringToDayOfWeek(broadcast.getBeginTime()) + DateUtilities.isoStringToTimeString(broadcast.getBeginTime()));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			holder.mChannelName.setText(broadcast.getChannel().getName());
+
+			if (programType != null) {
+				if (Consts.DAZOO_PROGRAM_TYPE_MOVIE.equals(programType)) {
+					holder.mDetails.setText(broadcast.getProgram().getGenre() + mActivity.getResources().getString(R.string.from) + broadcast.getProgram().getYear());
+				} else if (Consts.DAZOO_PROGRAM_TYPE_TV_EPISODE.equals(programType)) {
+					holder.mDetails.setText(mActivity.getResources().getString(R.string.season) + " " + broadcast.getProgram().getSeason().getNumber() + " "
+							+ mActivity.getResources().getString(R.string.episode) + " " + broadcast.getProgram().getEpisodeNumber());
+				} else if (Consts.DAZOO_PROGRAM_TYPE_SPORT.equals(programType)) {
+					holder.mDetails.setText(broadcast.getProgram().getSportType() + " " + broadcast.getProgram().getTournament());
+				} else if (Consts.DAZOO_PROGRAM_TYPE_OTHER.equals(programType)) {
+					holder.mDetails.setText(broadcast.getProgram().getCategory());
+				}
+			}
+
 		}
-		
+
 		holder.mContainer.setOnClickListener(new View.OnClickListener() {
 
 			@Override
@@ -87,25 +125,26 @@ public class BlockPopularListViewAdapter extends BaseAdapter{
 				intent.putExtra(Consts.INTENT_EXTRA_BROADCAST_BEGINTIMEINMILLIS, broadcast.getBeginTimeMillis());
 				intent.putExtra(Consts.INTENT_EXTRA_CHANNEL_ID, broadcast.getChannel().getChannelId());
 				// TODO GET THE DATE FROM BEGIN TIME OF THE BROADCAST
-				//intent.putExtra(Consts.INTENT_EXTRA_CHANNEL_CHOSEN_DATE, mDate.getDate());
-
+				// intent.putExtra(Consts.INTENT_EXTRA_CHANNEL_CHOSEN_DATE, mDate.getDate());
 
 				mActivity.startActivity(intent);
 				mActivity.overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
 			}
 		});
-		
-		
+
 		return rowView;
 	}
-	
-	private static class ViewHolder{
-		RelativeLayout mContainer;
-		ImageView mPoster;
-		TextView mTitle;
-		TextView mChannelName;
-		TextView mTime;
-		TextView mDetails;
-		ProgressBar mProgressBar;
+
+	private static class ViewHolder {
+		LinearLayout	mContainer;
+		ImageView		mPoster;
+		ProgressBar		mImageProgressBar;
+		ImageView		mIcon;
+		TextView		mTitle;
+		TextView		mTime;
+		TextView		mChannelName;
+		TextView		mDetails;
+		TextView		mProgressBarTitle;
+		ProgressBar		mProgressBar;
 	}
 }
