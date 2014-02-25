46d65179df884918655715a34eed2d8d4d2c9078
Alexander Cyon <alex.cyon@gmail.com>
2014-02-25 09:05:58 +0100

    Working on ProgresUtils

--- src/com/mitv/utilities/ProgressBarUtils.java
+++ src/com/mitv/utilities/ProgressBarUtils.java
@@ -17,6 +17,8 @@ import com.mitv.R;
 
 public class ProgressBarUtils
 {
+	private static final int MINUTES_PER_HOUR = 60;
+	
 	public static void setupProgressBar(Activity mActivity, TVBroadcast broadcast, ProgressBar progressBar, TextView progressTxt)
 	{
 		progressBar.setMax(broadcast.getBroadcastDurationInMinutes() + 1);
@@ -29,10 +31,10 @@ public class ProgressBarUtils
 		// different representation of "X min left" for Spanish and all other languages
 		if (Locale.getDefault().getLanguage().endsWith("es")) 
 		{
-			if (timeLeft > 60) 
+			if (timeLeft > MINUTES_PER_HOUR) 
 			{
-				int hours = timeLeft / 60;
-				int minutes = timeLeft - hours * 60;
+				int hours = timeLeft / MINUTES_PER_HOUR;
+				int minutes = timeLeft - hours * MINUTES_PER_HOUR;
 				
 				progressTxt.setText(mActivity.getResources().getQuantityString(R.plurals.left, hours) + " " + hours + " " + 
 						mActivity.getResources().getQuantityString(R.plurals.hour, hours) + " " + 
@@ -47,11 +49,11 @@ public class ProgressBarUtils
 		} 
 		else 
 		{
-			if (timeLeft > 60) 
+			if (timeLeft > MINUTES_PER_HOUR) 
 			{
-				int hours = timeLeft / 60;
+				int hours = timeLeft / MINUTES_PER_HOUR;
 				
-				int minutes = timeLeft - hours * 60;
+				int minutes = timeLeft - hours * MINUTES_PER_HOUR;
 				
 				progressTxt.setText(hours + " " + mActivity.getResources().getQuantityString(R.plurals.hour, hours) + " " + 
 						mActivity.getResources().getString(R.string.and) + " " + minutes + " " + 
