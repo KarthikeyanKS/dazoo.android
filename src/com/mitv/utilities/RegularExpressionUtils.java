02cbf2fc55b3467ceed0280b4f274dcc3ce97b34
Filipe <filipe.castro@millicom.com>
2014-04-03 16:59:21 +0200

    - Fixed the Search for shows starting with "la" no hits bug

--- src/com/mitv/utilities/RegularExpressionUtils.java
+++ src/com/mitv/utilities/RegularExpressionUtils.java
@@ -59,19 +59,4 @@ public abstract class RegularExpressionUtils
 		
 		return isValid;
 	}
-	
-	
-	public static String escapeSpaceChars(String stringWithSpaceChars) 
-	{
-		String stringWithEscapedSpaceChars = stringWithSpaceChars.replaceAll(" ", "%20");
-		
-		return stringWithEscapedSpaceChars;
-	}
-	
-	public static String decodeEncodedSpaceChars(String stringWithEncodedSpaceChars) 
-	{
-		String stringWithoutEncodedSpaceChars = stringWithEncodedSpaceChars.replaceAll("%20", " ");
-		
-		return stringWithoutEncodedSpaceChars;
-	}
 }
