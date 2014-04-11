3e499c45cda03cadb02ccf7f5ebec9011b616d06
Alexander Cyon <alex.cyon@gmail.com>
2014-04-11 14:29:34 +0200

    Fixed broken tests (again)

--- SecondScreenTest/src/com/mitv/test/gson/GetUserTVChannelIdsTest.java
+++ SecondScreenTest/src/com/mitv/test/gson/GetUserTVChannelIdsTest.java
@@ -20,8 +20,8 @@ import com.mitv.enums.HTTPRequestTypeEnum;
 import com.mitv.http.HTTPCoreResponse;
 import com.mitv.http.HeaderParameters;
 import com.mitv.http.URLParameters;
-import com.mitv.models.TVChannelId;
-import com.mitv.models.UserLoginData;
+import com.mitv.models.objects.mitvapi.TVChannelId;
+import com.mitv.models.objects.mitvapi.UserLoginData;
 
 
 
--- SecondScreenTest/src/com/mitv/test/gson/PerformUserRegistrationTest.java
+++ SecondScreenTest/src/com/mitv/test/gson/PerformUserRegistrationTest.java
@@ -15,8 +15,8 @@ import com.google.gson.JsonSyntaxException;
 import com.mitv.Constants;
 import com.mitv.enums.HTTPRequestTypeEnum;
 import com.mitv.http.HTTPCoreResponse;
-import com.mitv.models.UserLoginData;
 import com.mitv.models.gson.serialization.UserRegistrationData;
+import com.mitv.models.objects.mitvapi.UserLoginData;
 
 
 
