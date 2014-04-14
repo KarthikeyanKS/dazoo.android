b4542fc65c2ba57f3ac72809cee7b223cb4abbfd
Alexandra Tsampikakis <Alexandra.Tsampikakis@Millicom.com>
2014-04-14 16:36:20 +0200

    Bugfix: #1122 Filter Channel Selection Page - Clear on Submit and add (x)

--- src/com/mitv/activities/MyChannelsActivity.java
+++ src/com/mitv/activities/MyChannelsActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -50,6 +51,7 @@ public class MyChannelsActivity
 	private TextView channelCountTextView;
 
 	private EditText searchChannelField;
+	private TextView editTextClearBtn;
 	
 	private MyChannelsListAdapter adapter;
 	
@@ -100,8 +102,12 @@ public class MyChannelsActivity
 				TrackingGAManager.sharedInstance().sendUserPressedChannelInMyChannelsActivity();
 			}
 		});
+		
 		channelCountTextView = (TextView) findViewById(R.id.mychannels_header_counter_tv);
 		searchChannelField = (EditText) findViewById(R.id.mychannels_header_search_ev);
+		
+		editTextClearBtn = (TextView) findViewById(R.id.searchbar_clear_x);
+		editTextClearBtn.setOnClickListener(this);
 	}
 
 	
@@ -130,6 +136,7 @@ public class MyChannelsActivity
 	public void onBackPressed() 
 	{
 		super.onBackPressed();	
+		
 		finish();
 	}
 
@@ -283,7 +290,10 @@ public class MyChannelsActivity
 		channelsMatchingSearch.clear();
 		
 		if (!TextUtils.isEmpty(search)) 
-		{				
+		{
+			
+			editTextClearBtn.setVisibility(View.VISIBLE);
+			
 			/* Go through list of all channels and add channels which name contains the searched string */
 			for(TVChannel tvChannel : allChannelObjects) 
 			{
@@ -301,9 +311,34 @@ public class MyChannelsActivity
 		{
 			/* If search string is empty show all channel objects */
 			channelsMatchingSearch.addAll(allChannelObjects);
+			
+			editTextClearBtn.setVisibility(View.INVISIBLE);
 		}
 
 		adapter.setChannelsMatchingSearchAndRefreshAdapter(search, channelsMatchingSearch);
+	}
+	
+	
+	
+	@Override
+	public void onClick(View v) 
+	{
+		super.onClick(v);
+
+		switch (v.getId()) 
+		{
+			case R.id.searchbar_clear_x: 
+			{
+				searchChannelField.setText("");
+				
+				editTextClearBtn.setVisibility(View.INVISIBLE);
+				
+				/* Hide keyboard when pressing clean button */
+				InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(searchChannelField.getWindowToken(), 0);
+				break;
+			}
+		}
 	}
 
 	@Override
